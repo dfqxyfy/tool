9f1f24ce81d4737f9d3a55bad1e82f0da429abda
Administrator <crazyanyone@163.com>
2016-06-21 00:28:43 +0800

    springboot

---
+++ src/main/java/com/ccs/start/Application.java
@@ -0,0 +1,22 @@
+package com.ccs.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2016/6/21.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
