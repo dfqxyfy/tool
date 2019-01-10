78269df9c07bb5ecfeb5b91bc43ad8d0154b415c
ccs <chaichuanshi@sunlands.com>
2019-01-10 20:26:53 +0800

    leetcode

---
+++ tool/src/main/java/cn/letcode/ValidParenthesesSolution.java
@@ -0,0 +1,32 @@
+package cn.letcode;
+
+import java.util.Stack;
+
+public class ValidParenthesesSolution {
+    public boolean isValid(String s) {
+        Stack<String> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            String s1 = Character.toString(s.charAt(i));
+            if("(".equals(s1)||"{".equals(s1)||"[".equals(s1)){
+                stack.push(s1);
+            }else{
+                if(stack.empty()){
+                    return false;
+                }
+                String pop = stack.pop();
+                if(")".equals(s1) && !"(".equals(pop)){
+                        return false;
+                }else if("}".equals(s1) && !"{".equals(pop)){
+                        return false;
+                }else if("]".equals(s1) && !"[".equals(pop)){
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidParenthesesSolution().isValid("]"));
+    }
+}
