4e7f87a73e0dfaf08add6a8db9db7bf033988b59
dfqxyfy <crazyanyone@163.com>
2019-05-18 12:42:51 +0800

    test

--- spider/src/main/java/cn/ccs/spider/jsoup/TestJsoup.java
+++ spider/src/main/java/cn/ccs/spider/jsoup/TestJsoup.java
@@ -1,20 +1,20 @@
 package cn.ccs.spider.jsoup;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
-
-public class TestJsoup {
-
-    public static void main(String[] args) {
-        Connection connect = Jsoup.connect("https://www.baidu.com");
-        try {
-            Document document = connect.get();
-            System.out.println(document);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//
+//import org.jsoup.Connection;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//
+//import java.io.IOException;
+//
+//public class TestJsoup {
+//
+//    public static void main(String[] args) {
+//        Connection connect = Jsoup.connect("https://www.baidu.com");
+//        try {
+//            Document document = connect.get();
+//            System.out.println(document);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- spider/src/main/java/cn/ccs/spider/selenium/basic/BasicChromeSpider.java
+++ spider/src/main/java/cn/ccs/spider/selenium/basic/BasicChromeSpider.java
@@ -38,7 +38,14 @@ public class BasicChromeSpider {
 
     public static void main(String[] args) {
         BasicChromeSpider chrome = new BasicChromeSpider();
-        chrome.googleSearch("www.baidu.com");
+        chrome.driver.get("https://coinmarketcap.com/");
+
+            try {
+                Thread.sleep(8000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        chrome.driver.close();
     }
 
     private String initJs(Integer i, Integer count) {
@@ -50,11 +57,12 @@ public class BasicChromeSpider {
     public List googleSearch(String url) {
         //String url = "http://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?keywords=%E7%94%B5%E6%B1%A0&cosite=baidujj&location=&trackid=4014000002631796&p4pid=1464007107509033253184&se=&spm=a312h.7841636.1998813771.dsearch_1998813775_0";
         driver.get(url);
-        List<WebElement> list = driver.findElement(By.id("sm-maindata")).findElement(By.tagName("div"))
-                .findElement(By.tagName("ul")).findElements(By.tagName("li"));
+//        List<WebElement> list = driver.findElement(By.id("sm-maindata")).findElement(By.tagName("div"))
+//                .findElement(By.tagName("ul")).findElements(By.tagName("li"));
 
 
-        return list;
+//        return list;
+        return null;
     }
 
 }
