a8d91e525c44ad5004210a0bf5525372fae2bccd
dfqxyfy <crazyanyone@163.com>
2020-01-30 12:49:30 +0800

    java 8

--- sockets/src/main/java/cn/ccs/socket/MySocketServer.java
+++ sockets/src/main/java/cn/ccs/socket/MySocketServer.java
@@ -2,8 +2,13 @@ package cn.ccs.socket;
 
 import org.apache.log4j.net.SocketServer;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MySocketServer {
     public static void main(String[] args) {
         //SocketServer socketServer = new SocketServer();
+       AtomicInteger j = new AtomicInteger(0);
+      List list;
     }
 }
